1559ccf5b5b554c4438cb56ac0945c65b846dae3
deepak gahlot <deepakgahlot98@gmail.com>
2020-02-09 22:51:41 +0530

    adding viewpager
    Fragments 103 adding view pager to the code repo

---
+++ app/src/main/java/com/gahlot/fragments101/ViewPager/ModelObject.java
@@ -0,0 +1,28 @@
+package com.gahlot.fragments101.ViewPager;
+
+import com.gahlot.fragments101.R;
+
+public enum ModelObject {
+
+
+        RED(R.string.red, R.layout.view_red),
+        BLUE(R.string.blue, R.layout.view_blue),
+        GREEN(R.string.green, R.layout.view_green);
+
+        private int mTitleResId;
+        private int mLayoutResId;
+
+        ModelObject(int titleResId, int layoutResId) {
+            mTitleResId = titleResId;
+            mLayoutResId = layoutResId;
+        }
+
+        public int getTitleResId() {
+            return mTitleResId;
+        }
+
+        public int getLayoutResId() {
+            return mLayoutResId;
+        }
+
+}
